a37e8dc4585265d77a2ed76d4bc995f0ae5325a4
chenqizheng <2503227577@qq.com>
2017-12-06 16:45:52 +0800

    none

--- src/zibiao/zb.py
+++ src/zibiao/zb.py
@@ -2,6 +2,25 @@
 import pandas as pd
 
 
+
+def xyz(p1, p2, tp):
+    """
+    (p1.x - p2.x) / (p1.y - p2.y) * (p.y - p2.y) + p2.x
+    点在线的左边还是右边
+    左=true, 右=false
+
+    """
+
+    x1, y1 = p1
+    x2, y2 = p2
+
+    x, y = tp
+
+    #tmp = (y1 – y2) *x + (x2 – x1) *y + x1 * y2 – x2 * y1
+    tmpx = (x1-x2) / (y1-y2) * (y-y2) + x2
+    if tmpx > x: return True
+    return False
+
 class ZB(object):
 
     @classmethod
@@ -251,8 +270,27 @@ class ZB(object):
         # tmp_df = pd.concat([df, kdj_df], axis=1) # 横向合并
         return kdj_df.loc[:, ["signal"]]
 
+    @classmethod
+    def ma_buy_signal2(cls, df):
 
+        names = ["ma5_10", "ma10_20", "ma5_20", "ma_desc"]
+        df = df.ix[:, names]
+        df['fii1'] = 0
 
+        p1 = (-25, -3.5)
+        p2 = (-5.5, -19)
+
+        for idd, row in df.iterrows():
+            tp = (row['ma10_20'], row['ma5_10'])
+            flg = xyz(p1, p2, tp)
+            if flg:
+                df.loc[idd, "fii1"] = 1
+
+        signal_df = df.ix[(df['fii1'] == 1) | (df['ma5_20'] <= -25) | (df['ma_desc'] <= -13), names]
+
+        signal_df['signal2'] = 1
+
+        return signal_df
 
 
 
